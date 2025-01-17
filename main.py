942183c6b99b2092aa890954660d146c71662ea3
YusiferZendric <yzendric@gmail.com>
2025-01-17 17:11:32 +0530

    path Parameters go thorugh

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return "Hello Aditya!"
+    return "Welcome to Book Store!"
+
+# @app.get("/price/{price}")
+# def cost(price):
+#     return f"The Book's Price is ${price}"
+@app.get("/price/{price}")
+def cost(price:int):
+    return f"The Book's Price is ${price}"
